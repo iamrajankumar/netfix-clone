3ce7fcd2e46f0bfddd1e774a11d7241a12989cf1
iamrajankumar <rajankumarmall9@gmail.com>
2022-09-02 10:20:17 +0530

    netflix clone

---
+++ netfli.js
@@ -0,0 +1,725 @@
+let movies = [
+  {
+      Name: 'Masoom',
+      Type: 'Bollywood',
+      rating: 4,
+      collection: "100c",
+      img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+      relaeseYear: 2018
+  },
+  {
+      Name: 'Ms Marvel',
+      Type: 'hollywood',
+      rating: 8,
+      collection: "1000c",
+      img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+  },
+  {
+      Name: 'Baaghi',
+      Type: 'bollywood',
+      rating: 5,
+      collection: "200c",
+      img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6536/846536-v',
+  },
+  {
+      Name: 'M.S.Dhoni',
+      Type: 'bollywood',
+      rating: 9,
+      collection: "300c",
+      img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v',
+  },
+  {
+      Name:'Ghar Waapsi',
+      Type:'bollywood',
+      rating:5.5,
+      collection:'250c',
+      img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7377/1307377-v-325f72982f0f',
+  },
+
+  {
+    Name:'Parampara',
+    Type:'bollywood',
+    rating:6,
+    collection:'100c',
+    img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7295/1307295-v-4d6248ec72a4',
+},
+
+{
+  Name:'Vikram',
+  Type:'bollywood',
+  rating:7.5,
+  collection:'600c',
+  img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6253/1296253-v-3a1a10046b46',
+},
+
+{
+  Name: 'Mahabharat',
+  Type: 'Bollywood',
+  rating: 10,
+  collection: "1000c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7527/267527-v',
+
+},
+{
+  Name: 'Mahadev',
+  Type: 'Bollywood',
+  rating: 8,
+  collection: "500c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2876/862876-v",
+},
+{
+  Name: 'radhakirshna',
+  Type: 'Bollywood',
+  rating: 5,
+  collection: "200c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8946/38946-v',
+},
+{
+  Name: 'emali',
+  Type: 'Bollywood',
+  rating: 9,
+  collection: "300c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+Name: 'yeh rishta',
+Type: 'Bollywood',
+rating: 5.5,
+collection: "50l",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4516/1074516-v-2f4f07b1e4ec',
+},
+
+{
+Name: 'Anupama',
+Type: 'Bollywood',
+rating: 3,
+collection: "350c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8774/1028774-v-85c7d8f07559',
+},
+
+{
+Name: 'Shoorbeer',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5126/1305126-v-5f2e72c8303b',
+},
+
+{
+  Name: 'Housefull',
+  Type: 'Bollywood',
+  rating: 7,
+  collection: "50c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+  Name: 'Baby',
+  Type: 'Bollywood',
+  rating: 3,
+  collection: "150c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+  Name: 'Shang-Chi',
+  Type: 'Hollywood',
+  rating: 9,
+  collection: "2000c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+  Name: 'Tanaji',
+  Type: 'Bollywood',
+  rating: 5,
+  collection: "300c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+  Name: 'Housefull',
+  Type: 'Bollywood',
+  rating: 7,
+  collection: "50c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+  Name: 'Baby',
+  Type: 'Bollywood',
+  rating: 3,
+  collection: "150c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+  Name: 'Shang-Chi',
+  Type: 'Hollywood',
+  rating: 9,
+  collection: "2000c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+  Name: 'Tanaji',
+  Type: 'Bollywood',
+  rating: 5,
+  collection: "300c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+{
+  Name: 'Masoom',
+  Type: 'Bollywood',
+  rating: 4,
+  collection: "100c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+  relaeseYear: 2018
+},
+{
+  Name: 'Ms Marvel',
+  Type: 'hollywood',
+  rating: 8,
+  collection: "1000c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+},
+
+{
+  Name: 'Masoom',
+  Type: 'Bollywood',
+  rating: 4,
+  collection: "100c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+  relaeseYear: 2018
+},
+{
+  Name: 'Ms Marvel',
+  Type: 'hollywood',
+  rating: 8,
+  collection: "1000c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+},
+{
+  Name: 'Baaghi',
+  Type: 'bollywood',
+  rating: 5,
+  collection: "200c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6536/846536-v',
+},
+{
+  Name: 'M.S.Dhoni',
+  Type: 'bollywood',
+  rating: 9,
+  collection: "300c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v',
+},
+{
+  Name:'Ghar Waapsi',
+  Type:'bollywood',
+  rating:5.5,
+  collection:'250c',
+  img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7377/1307377-v-325f72982f0f',
+},
+
+{
+Name:'Parampara',
+Type:'bollywood',
+rating:6,
+collection:'100c',
+img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7295/1307295-v-4d6248ec72a4',
+},
+
+{
+Name:'Vikram',
+Type:'bollywood',
+rating:7.5,
+collection:'600c',
+img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6253/1296253-v-3a1a10046b46',
+},
+
+{
+Name: 'Mahabharat',
+Type: 'Bollywood',
+rating: 10,
+collection: "1000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7527/267527-v',
+
+},
+{
+Name: 'Mahadev',
+Type: 'Bollywood',
+rating: 8,
+collection: "500c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2876/862876-v",
+},
+{
+Name: 'radhakirshna',
+Type: 'Bollywood',
+rating: 5,
+collection: "200c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8946/38946-v',
+},
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+Name: 'yeh rishta',
+Type: 'Bollywood',
+rating: 5.5,
+collection: "50l",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4516/1074516-v-2f4f07b1e4ec',
+},
+
+{
+Name: 'Anupama',
+Type: 'Bollywood',
+rating: 3,
+collection: "350c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8774/1028774-v-85c7d8f07559',
+},
+
+{
+Name: 'Shoorbeer',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5126/1305126-v-5f2e72c8303b',
+},
+
+{
+Name: 'Housefull',
+Type: 'Bollywood',
+rating: 7,
+collection: "50c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+Name: 'Baby',
+Type: 'Bollywood',
+rating: 3,
+collection: "150c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+Name: 'Shang-Chi',
+Type: 'Hollywood',
+rating: 9,
+collection: "2000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+Name: 'Tanaji',
+Type: 'Bollywood',
+rating: 5,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+Name: 'Housefull',
+Type: 'Bollywood',
+rating: 7,
+collection: "50c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+Name: 'Baby',
+Type: 'Bollywood',
+rating: 3,
+collection: "150c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+Name: 'Shang-Chi',
+Type: 'Hollywood',
+rating: 9,
+collection: "2000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+Name: 'Tanaji',
+Type: 'Bollywood',
+rating: 5,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+{
+Name: 'Masoom',
+Type: 'Bollywood',
+rating: 4,
+collection: "100c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+relaeseYear: 2018
+},
+{
+Name: 'Ms Marvel',
+Type: 'hollywood',
+rating: 8,
+collection: "1000c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+},
+
+{
+  Name: 'Masoom',
+  Type: 'Bollywood',
+  rating: 4,
+  collection: "100c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+  relaeseYear: 2018
+},
+{
+  Name: 'Ms Marvel',
+  Type: 'hollywood',
+  rating: 8,
+  collection: "1000c",
+  img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+},
+{
+  Name: 'Baaghi',
+  Type: 'bollywood',
+  rating: 5,
+  collection: "200c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6536/846536-v',
+},
+{
+  Name: 'M.S.Dhoni',
+  Type: 'bollywood',
+  rating: 9,
+  collection: "300c",
+  img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v',
+},
+{
+  Name:'Ghar Waapsi',
+  Type:'bollywood',
+  rating:5.5,
+  collection:'250c',
+  img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7377/1307377-v-325f72982f0f',
+},
+
+{
+Name:'Parampara',
+Type:'bollywood',
+rating:6,
+collection:'100c',
+img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7295/1307295-v-4d6248ec72a4',
+},
+
+{
+Name:'Vikram',
+Type:'bollywood',
+rating:7.5,
+collection:'600c',
+img:'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6253/1296253-v-3a1a10046b46',
+},
+
+{
+Name: 'Mahabharat',
+Type: 'Bollywood',
+rating: 10,
+collection: "1000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7527/267527-v',
+
+},
+{
+Name: 'Mahadev',
+Type: 'Bollywood',
+rating: 8,
+collection: "500c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/2876/862876-v",
+},
+{
+Name: 'radhakirshna',
+Type: 'Bollywood',
+rating: 5,
+collection: "200c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8946/38946-v',
+},
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+Name: 'yeh rishta',
+Type: 'Bollywood',
+rating: 5.5,
+collection: "50l",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/4516/1074516-v-2f4f07b1e4ec',
+},
+
+{
+Name: 'Anupama',
+Type: 'Bollywood',
+rating: 3,
+collection: "350c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8774/1028774-v-85c7d8f07559',
+},
+
+{
+Name: 'Shoorbeer',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5126/1305126-v-5f2e72c8303b',
+},
+
+{
+Name: 'Housefull',
+Type: 'Bollywood',
+rating: 7,
+collection: "50c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+Name: 'Baby',
+Type: 'Bollywood',
+rating: 3,
+collection: "150c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+Name: 'Shang-Chi',
+Type: 'Hollywood',
+rating: 9,
+collection: "2000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+Name: 'Tanaji',
+Type: 'Bollywood',
+rating: 5,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+
+{
+Name: 'Housefull',
+Type: 'Bollywood',
+rating: 7,
+collection: "50c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3669/593669-v',
+
+},
+{
+Name: 'Baby',
+Type: 'Bollywood',
+rating: 3,
+collection: "150c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7416/957416-v",
+},
+{
+Name: 'Shang-Chi',
+Type: 'Hollywood',
+rating: 9,
+collection: "2000c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8723/1078723-v-a1a04c7474f9',
+},
+{
+Name: 'Tanaji',
+Type: 'Bollywood',
+rating: 5,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7676/647676-v',
+},
+
+{
+Name: 'Super 30',
+Type: 'Bollywood',
+rating: 9,
+collection: "500c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6828/556828-v',
+},
+
+{
+Name: 'Total Dhamaal',
+Type: 'Bollywood',
+rating: 6,
+collection: "3c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8285/388285-v',
+},
+
+{
+Name: 'emali',
+Type: 'Bollywood',
+rating: 9,
+collection: "300c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8189/1248189-v-f4e1cbe767aa',
+},
+{
+Name: 'Masoom',
+Type: 'Bollywood',
+rating: 4,
+collection: "100c",
+img: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/250/1280250-v-d2703f3616e1',
+relaeseYear: 2018
+},
+{
+Name: 'Ms Marvel',
+Type: 'hollywood',
+rating: 8,
+collection: "1000c",
+img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9317/1269317-v-c851767a526b",
+},
+];
+
+let movieList = document.getElementById('movieList');
+
+movies.forEach((list) => {
+  let li = document.createElement('li');
+  li.innerHTML = `<div class = 'movie'>
+  <div class = 'movie-img'>
+  <img src = '${list.img}'
+  </div>
+  <div class = "movie-name"> Name :
+  ${list.Name}
+  </div>
+  <div class = "movie-name">Type :
+  ${list.Type}
+  </div>
+  <div class = "movie-name">Rating :
+  ${list.rating}
+  </div>
+  <div class = "movie-name">Collection :
+  ${list.collection}
+  </div>`
+  movieList.appendChild(li);
+
+}
+
+);
+
+function leaveChange(){
+
+  let listMovies = document.getElementById("leave").value;
+  console.log(listMovies);
+}
+
+leaveChange();
